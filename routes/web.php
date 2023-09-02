0c1b8cb0d1f4c5f2d1d4f6ef23cd5c78ec7fd4f8
Ahmed zaqowt <ahmadzaqowt@gmail.com>
2023-09-02 19:35:10 +0300

    blade and MVC

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,27 +18,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-
+/*
 
 Route::get('users' , function(){
     return 'users' ;
 });
-/*
+
 Route::get('users/profile/{name}' , function($name){
     return 'users profile ' . $name ;
 });
 
 Route::get('users/profile' , function(){
     return 'users profile ' ;
-});*/
-/*
+});
+
 Route::get('users/profile/{name?}' , function($name = null){
     if($name == null){
        return 'response null' ;
     }else{
      return 'users profile ' . $name ;
     }
-});*/
+});
 
 
 Route::get('news/{id?}' , function($id = null){
@@ -49,12 +50,46 @@ Route::get('news/{id?}' , function($id = null){
     return 'all news ... ' ;
 });
 
-
-
-
-
 Route::put('put' , function(){});
 Route::delete('delete' , function(){});
+
+Route::get('about' , function(){
+    return 'about us' ;
+});
+
+Route::get('index' , function(){
+    return view('index');
+});*/
+
+
+
+
+// site 1  .. site1/home
+
+
+
+Route::get('/' , function () {
+   return view('index') ;
+});
+
+Route::get('home' , function(){
+   return view('site1.home');
+});
+
+Route::prefix('site1')->controller(SiteController::class)
+->name('site.')->group(function(){
+    Route::get('/index' ,   'index')->name('index');
+    Route::get('/about' ,  'about')->name('about');
+    Route::get('/services' ,  'services')->name('services');
+    Route::get('/contact' ,  'contact')->name('contact');
+});
+
+
+
+
+
+
+
 
 
 
